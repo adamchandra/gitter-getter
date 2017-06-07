b6fd2a9e31b73a257f18201bd062a24360a33f34
Adam Saunders <saunders@cs.umass.edu>
2017-06-07 10:03:14 -0400

    ()

---
+++ build.sbt
@@ -0,0 +1,3 @@
+
+name := "gitter-getter"
+scalaVersion := "2.11.9"
---
+++ src/main/scala/gittergetter/getgitter.scala
@@ -0,0 +1,12 @@
+package gittergetter
+
+
+object getGitter extends App {
+
+  // curl -i -H "Accept: application/json" -H "Authorization: Bearer ff9076c36e999ca2df940e77c6967b468dc67e3c" "https://api.gitter.im/v1/rooms/$roomid/chatMessages?limit=50" | tail -n 1 > tmp.json
+  // python -m json.tool tmp.json > "$roomname.chatMessages.json"
+  // # beforeId
+  override def main(args: Array[String]) = {
+
+  }
+}
